545d2af3b6d49c7cf796d8652b05d4f879e7cadf
Yejin <yejinkim5979@gmail.com>
2022-02-14 23:19:29 -0500

    made square and board

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import Board from './components/Board';
 
 const App = () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+  <div>
+    <h1>Tic Tac Toe</h1>
+    <Board />
+  </div>
 );
 
 export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = ({ value }) => {
+  return <button type="button"> {value}</button>;
+};
+//rafce
+export default Square;
